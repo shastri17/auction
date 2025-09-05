0bd6d4d40f4c02582c7e7ed8f10c14b3c7c53949
shastri17 <ganesh.s.shastri@gamil.com>
2025-09-05 21:08:30 +0530

    fixed

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9999'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://13.234.48.132:9999'
 
 // Create axios instance with default config
 const api = axios.create({
